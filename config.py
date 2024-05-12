3c670c3f11cf4b449f8ae05247157c9a4b8a3810
belaevprohor1 <151851555+belaevprohor1@users.noreply.github.com>
2024-05-12 19:22:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = "6487775773:AAFSEar3wo8HRfEDPDbiEBK5UMCR6Oef8sE"
+FOLDER_ID =  "b1g7i6isteuorfoua1f5"
+IAM_TOKEN = "t1.9euelZqeyJmPzM-WkI_HyZiMmpGLlO3rnpWanZeVi8nHmJKJlcacyMeSipbl9PdtM3xN-e8Ze0er3fT3LWJ5TfnvGXtHq83n9euelZqVnMnPnsmPz8idxo-LzI-ene_8xeuelZqVnMnPnsmPz8idxo-LzI-enb3rnpWakpiYzYrIyJDJlJqNjomWk5O13oac0ZyQko-Ki5rRi5nSnJCSj4qLmtKSmouem56LntKMng.AT1lg6DLgDBMxv47Q_aW6UrWLrmDF1EIsPaD5suTAQTC-lYv626NTZcMl2EAMKFTVgFpbO-3NH63TbeZ5phVBA"
+ADMINS_IDS = "5572552869"
+MAX_USERS = 3
+MAX_GPT_TOKENS = 400
+COUNT_LAST_MSG = 4
+TOKENIZE_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
+GPT_MODEL = 'yandexgpt-lite'
+GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+MAX_USER_STT_BLOCKS = 10
+MAX_USER_TTS_SYMBOLS = 5_000
+MAX_USER_GPT_TOKENS = 2_000
+
+LOGS = 'creds/logs.log'
+DB_FILE = 'messages.db'
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Будь добрым другом пользователя'}]
